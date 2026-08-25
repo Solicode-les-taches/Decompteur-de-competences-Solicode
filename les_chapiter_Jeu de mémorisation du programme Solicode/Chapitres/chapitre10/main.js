6d059e85ffdde63388e30a7a1f3efee06a47811d
Oussama Ech-charef <oussamacharef71@gmail.com>
2026-08-25 19:14:01 +0100

    Update main.js

--- les_chapiter_Jeu de mémorisation du programme Solicode/Chapitres/chapitre10/main.js
+++ les_chapiter_Jeu de mémorisation du programme Solicode/Chapitres/chapitre10/main.js
@@ -2,4 +2,8 @@ for (let i = 1; i <= 10; i++) {
     if (i === 5) continue; // sauter le 5
     console.log("Nombre : " + i);
   }
+  for (let i = 2; i <= 19; i++) {
+    if (i === 5) continue; // sauter le 5
+    console.log("Nombre : " + i);
+  }
   
