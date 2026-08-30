f8b340e113e27080d6283053b8ccc5e9de1d475d
Oussama Ech-charef <oussamacharef71@gmail.com>
2026-08-30 22:52:24 +0100

    Simplify number printing by removing extra loops
    Removed redundant loops that print numbers skipping 5.

--- les_chapiter_Jeu de mémorisation du programme Solicode/Chapitres/chapitre10/main.js
+++ les_chapiter_Jeu de mémorisation du programme Solicode/Chapitres/chapitre10/main.js
@@ -2,12 +2,4 @@ for (let i = 1; i <= 10; i++) {
     if (i === 5) continue; // sauter le 5
     console.log("Nombre : " + i);
   }
-  for (let i = 2; i <= 19; i++) {
-    if (i === 5) continue; // sauter le 5
-    console.log("Nombre : " + i);
-  }
-  for (let i = 5; i <= 19; i++) {
-    if (i === 5) continue; // sauter le 5
-    console.log("Nombre : " + i);
-  }
   
